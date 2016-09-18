6073cbc81e1e37cc25c86ad01d9e9eeefe1c5a44
liannah <liannahovhannisyan7@gmail.com>
2016-09-18 16:18:47 +0400

    first commit

--- factorial.js
+++ factorial.js
@@ -1,9 +1,14 @@
-average=0
-grayscale_img=img
-for i in range(len(img)):
-    for j in range(len(img[i])):
-        for k in range(len(img[i][j])):
-        average += img[i][j][k]
-        average=/3
-        grayscale_img[i][j][k]=average
-        plt.imshow(grayscale_img)
+<script>
+
+  const factorial= function(n) {
+    if ( n === 0 || n===1 ) {
+      return 1; 
+    }
+    else { 
+      return n*factorial(n-1);
+    }
+  };
+  
+  console.log(factorial(5));
+  
+</script>
